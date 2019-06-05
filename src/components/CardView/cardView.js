6920fb6de7a91c2b52e41213ac5b9c68da138102
Anshul Dubey <anshuldubey2166@gmail.com>
2019-06-06 00:03:01 +0530

    Added project card

--- src/components/CardView/cardView.js
+++ src/components/CardView/cardView.js
@@ -1,13 +1,19 @@
-import React,{Component} from "react";
+import React, { Component } from 'react';
+import DataCard from '../DataCard/dataCard';
 
-class CardView extends Component{
-    render(){
-        return(
-            <div className="card-view-wrapper">
-                This is card view
-            </div>
-        );
-    }
+class CardView extends Component {
+	render() {
+		return (
+			<div className="card-view-wrapper">
+				This is card view
+				<DataCard
+					title="Project Scope Management"
+					description="Project Scope Management refers to the set of processes that ensure a project’s scope is accurately defined and mapped. "
+                    image_location="https://www.simplilearn.com/ice9/free_resources_article_thumb/Project-Scope-Management-Cover.jpg"
+				/>
+			</div>
+		);
+	}
 }
 
 export default CardView;
