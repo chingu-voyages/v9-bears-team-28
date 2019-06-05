afb79ddd26a6e10f506f2649e75009c990f323a1
Anshul Dubey <anshuldubey2166@gmail.com>
2019-06-05 23:43:18 +0530

    Added router

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,13 +1,17 @@
-import React,{Component} from "react";
+import React, { Component } from 'react';
+import { Route, withRouter, Switch } from 'react-router-dom';
+import CardView from '../CardView/cardView';
 
-class App extends Component{
-    render(){
-        return(
-            <div className="app">
-                Welcome to app
-            </div>
-        );
-    }
+class App extends Component {
+	render() {
+		return (
+			<div className="app">
+				<Switch>
+					<Route exact path="/projects-view" component={CardView} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/CardView/cardView.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+
+class CardView extends Component{
+    render(){
+        return(
+            <div className="card-view-wrapper">
+                This is card view
+            </div>
+        );
+    }
+}
+
+export default CardView;
