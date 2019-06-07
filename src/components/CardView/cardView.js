7a1cae8aafefe1da46d85b5f1722338d73ae7e12
Anshul Dubey <anshuldubey2166@gmail.com>
2019-06-08 00:47:09 +0530

    Utility row added

--- src/components/CardView/cardView.js
+++ src/components/CardView/cardView.js
@@ -1,11 +1,100 @@
 import React, { Component } from 'react';
 import DataCard from '../DataCard/dataCard';
 import Grid from '@material-ui/core/Grid';
+import Dropdown from 'react-dropdown';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	search: {
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: fade(theme.palette.common.white, 0.15),
+		'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+		},
+		marginRight: theme.spacing(2),
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing(3),
+			width: 'auto',
+		},
+	},
+	searchIcon: {
+		width: theme.spacing(7),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	inputRoot: {
+		color: 'inherit',
+	},
+	inputInput: {
+		padding: theme.spacing(1, 1, 1, 7),
+		transition: theme.transitions.create('width'),
+		width: '100%',
+		[theme.breakpoints.up('md')]: {
+			width: 200,
+		},
+	},
+});
+
+const options = [{ value: 'one', label: 'Title' }, { value: 'two', label: 'Description' }];
+
+const SortDropdown = () => <Dropdown options={options} placeholder="Select an option" />;
+
+const order=[{value:'ascending',label:"Ascending"},{value:'descending',label:"Descending"}]
+
+const OrderSort = () => <Dropdown options={order} placeholder="Select an option" />;
+
+const UtilityRow = ({ classes }) => {
+	return (
+		<Grid container spacing={24}>
+			<Grid item md={5}>
+				<SearchBox classes={classes} />
+			</Grid>
+			<Grid item md={1} />
+			<Grid item md={2}>
+				<SortDropdown />
+			</Grid>
+			<Grid item md={1} />
+			<Grid item md={2}>
+				<OrderSort />
+			</Grid>
+		</Grid>
+	);
+};
+
+const SearchBox = ({ classes }) => (
+	<div className={classes.search}>
+		<div className={classes.searchIcon}>
+			<SearchIcon />
+		</div>
+		<InputBase
+			placeholder="Search project"
+			// value={this.state.searchText}
+			// onChange={this.onSearchTextChange}
+			// onKeyPress={this._handleKeyPress}
+			classes={{
+				root: classes.inputRoot,
+				input: classes.inputInput,
+			}}
+		/>
+	</div>
+);
 
 class CardView extends Component {
 	render() {
+		const { classes } = this.props;
 		return (
 			<div className="card-view-wrapper">
+				<UtilityRow classes={classes} />
 				<Grid container spacing={24}>
 					<Grid item md={4}>
 						<DataCard
@@ -31,4 +120,4 @@ class CardView extends Component {
 	}
 }
 
-export default CardView;
+export default withStyles(styles)(CardView);
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './components/App/app';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
+import 'react-dropdown/style.css';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
