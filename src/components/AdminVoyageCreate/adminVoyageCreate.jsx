5676edf99c8e31ee9c59a09ffe247d333b2a2dad
Anshul Dubey <anshuldubey2166@gmail.com>
2019-06-23 22:57:02 +0530

    UI done for admin form

--- src/components/AdminVoyageCreate/adminVoyageCreate.jsx
+++ src/components/AdminVoyageCreate/adminVoyageCreate.jsx
@@ -1,28 +1,53 @@
-import React from 'react';
-import './adminVoyageCreate.css';
+import React, { useState } from 'react';
 import FormField from '../Common/FormField/formField';
 import DatePicker from '../Common/DatePicker/datePicker';
-import { Button } from '@material-ui/core';
+import './adminVoyageCreate.css';
 
-function submitVoyageForm() {}
+function validateForm(fields) {
+	let { title, description } = fields;
+	return title && description ? true : false;
+}
 
 export default function AdminVoyageCreate() {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const submitVoyageForm = event => {
+		event.preventDefault();
+		const valid = validateForm(title, description);
+		if (valid) {
+			console.log({ title, description, startDate, endDate });
+		} else {
+			console.log('Invalid value');
+			console.log({ title, description, startDate, endDate });
+		}
+	};
+
 	return (
 		<div className="admin-voyage-create">
 			<div className="card create-voyage-card">
+				<h3 className="text-center mb-5">Create voyage</h3>
 				<form className="create-voyage-form" onSubmit={submitVoyageForm}>
-					<FormField title="Voyage title" placeholder="title" type="text" />
-					<FormField title="Voyage description" placeholder="Info about the voyage" type="text" />
-					<FormField title="Start date" customComponent={<DatePicker />} />
-					<FormField title="End date" customComponent={<DatePicker />} />
+					<FormField title="Voyage title" placeholder="title" type="text" value={title} onChange={setTitle} />
 					<FormField
-						title="Participation sheet"
-						placeholder="Select file"
-						type="file"
-						classFromProps="form-control-file"
+						title="Voyage description"
+						placeholder="Info about the voyage"
+						type="text"
+						value={description}
+						onChange={setDescription}
 					/>
-					<div className="col-xs-10 col-md-4 col-lg-2 btn-lg button-center">
-						<button type="button" class="btn btn-primary btn-block">
+					<FormField
+						title="Start date"
+						customComponent={<DatePicker value={startDate} onChange={setStartDate} />}
+					/>
+					<FormField
+						title="End date"
+						customComponent={<DatePicker value={endDate} onChange={setEndDate} />}
+					/>
+					<div className="col-xs-10 col-md-5 col-lg-2 btn-lg button-center">
+						<button type="submit" className="btn btn-primary btn-block btn-oval">
 							Submit
 						</button>
 					</div>
--- src/components/Common/FormField/formField.jsx
+++ src/components/Common/FormField/formField.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 
 export default function FormField(props) {
 	let classFromProps = props.classFromProps ? props.classFromProps : 'form-control';
+	console.log(props);
 	return (
-		<div class="form-group row">
-			<label for="inputEmail3" class="offset-sm-2 col-sm-4 col-form-label">
+		<div className="form-group row">
+			<label htmlFor="inputEmail3" className="offset-sm-2 col-sm-4 col-form-label">
 				{props.title}
 			</label>
-			<div class="col-sm-6">
+			<div className="col-sm-5">
 				{props.customComponent ? (
 					props.customComponent
 				) : (
-					<input type={props.type} className={classFromProps} placeholder={props.placeholder} />
+					<input
+						type={props.type}
+						className={classFromProps}
+						placeholder={props.placeholder}
+						value={props.value}
+						onChange={e => props.onChange(e.target.value)}
+					/>
 				)}
 			</div>
 		</div>
